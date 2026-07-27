f437050a80c490d2809c75e9e2afc821fefb1c90
Hamamunashe Tirekerwi <hamamunashetirekerwi@gmail.com>
2026-07-27 14:46:14 +0200

    another batch added

---
+++ lib/models/chicken_batch.dart
@@ -0,0 +1,39 @@
+class ChickenBatch{
+  final int? id;
+  final String batchName;
+  final String breed;
+  final int quantity;
+  final String dateReceived;
+  final double costPerBird;
+
+  ChickenBatch({
+    this.id,
+    required this.batchName,
+    required this.breed,
+    required this.quantity,
+    required this.dateReceived,
+    required this.costPerBird,
+  });
+
+  Map<String, dynamic> toMap(){
+    return {
+      'id': id,
+      'batchName': batchName,
+      'breed': breed,
+      'quantity': quantity,
+      'dateReceived': dateReceived,
+      'costPerBird': costPerBird,
+    };
+  }
+
+  factory ChickenBatch.fromMap(Map<String, dynamic> map){
+    return ChickenBatch(
+      id: map['id'],
+      batchName: map['batchName'],
+      breed: map['breed'],
+      quantity: map['quantity'],
+      dateReceived: map['dateReceived'],
+      costPerBird: map['costPerBird'],
+    );
+  }
+}
