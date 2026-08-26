5091d9535a485246886360508e5440698d5f31d5
Hamamunashe Tirekerwi <hamamunashetirekerwi@gmail.com>
2026-08-26 14:26:14 +0200

    Groq api

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,7 +1,7 @@
 class AppConstants {
   // Replace with your own API key
   static const String groqApiKey =
-      "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";
+      "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";
 
   // Recommended Groq model
   static const String model =
